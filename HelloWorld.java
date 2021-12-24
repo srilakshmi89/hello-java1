e35efa4e1d2acda5c53b85fd1b45e83679bdd02d
srilakshmi89 <v.srilakshmi@gmail.com>
2021-12-24 09:45:39 +0000

    wishes

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
     System.out.println("Happy cristamas");
+    System.out.println("Happy new year 2022");
   }
 }
